ff74d727b6cbd6a8bb03135db5af6ae978d0c22c
lucas <luccleandro@hotmail.com>
2024-05-19 17:09:07 -0300

    Checkpoint 05/19 - Almost finished modelling

---
+++ README.md
@@ -0,0 +1,16 @@
+# Music Analysis
+
+This is a entirely egotistical project where I brag about my musical taste.
+
+
+Data was retrieved from Last.fm, using [Chan's data export](https://mainstream.ghan.nl/export.)  for pure laziness of setting up the Last.fm API
+
+
+My Last.fm profile is [saintcecilla](https://www.last.fm/user/saintcecilla)
+
+___
+## Warning for the statisticians
+Even though I've applied some significance tests, most of what I used could be considered wrong if compared to the "correct statistical approach", like disregarding Linear Regression's assumptions and applying it either way.
+
+I'm forecasting, not applying causality methods, and forecasting methods are determined by the ones that work, not the one statistically correct.
+Please refer to: https://robjhyndman.com/papers/sst2.pdf .
---
+++ utils.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from scipy.signal import periodogram
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.tsa.arima.model import ARIMA
+
+def read_csv_properly(path: str) -> pd.DataFrame:
+	data = pd.read_csv(path, usecols=["utc_time", "artist", "album", "track"]
+		).rename(columns={"utc_time": "date_played"})
+
+	data["date_played"] = pd.to_datetime(
+		data["date_played"], format="%d %b %Y, %H:%M")
+
+	return data
+
+
+def get_music_time_series(data: pd.DataFrame, frequency: str) -> pd.Series:
+	data = data.set_index("date_played").groupby(pd.Grouper(freq=frequency)
+		).agg({"artist": "nunique", "album": "nunique", "track": "count"})
+	return data
+
+
+def plot_periodogram(ts: pd.Series, detrend='linear', ax=None):
+	fs = pd.Timedelta("365D") / pd.Timedelta("1D")
+	freqencies, spectrum = periodogram(
+		ts,
+		fs=fs,
+		detrend=detrend,
+		window="boxcar",
+		scaling='spectrum',
+	)
+	if ax is None:
+		_, ax = plt.subplots()
+	ax.step(freqencies, spectrum, color="purple")
+	ax.set_xscale("log")
+	ax.set_xticks([1, 2, 4, 6, 12, 26, 52, 104])
+	ax.set_xticklabels(
+		[
+			"Annual (1)",
+			"Semiannual (2)",
+			"Quarterly (4)",
+			"Bimonthly (6)",
+			"Monthly (12)",
+			"Biweekly (26)",
+			"Weekly (52)",
+			"Semiweekly (104)",
+		],
+		rotation=30,
+	)
+	ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+	ax.set_ylabel("Variance")
+	ax.set_title("Periodogram")
+	return ax
+
+
+def test_augmented_df(series: pd.Series) -> None:
+	adf = sm.tsa.adfuller(series,autolag=None, maxlag=4)
+	print("adf : ",adf[0])
+	print("p-value : ", adf[1])
+
+
+def get_optimized_arima(data: pd.Series):
+	aic = 0
+	persist_model = None
+
+	for ar_order in range(0,5):
+		for ma_order in range(1,6):
+			for diff_order in range(0,5):
+				model = ARIMA(data, order=(ar_order, diff_order, ma_order)).fit()
+				
+				if model.aic < aic:
+					aic = model.aic
+					persist_model = model
+
+	return persist_model
